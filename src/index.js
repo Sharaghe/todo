0390d50d26382f12ffac4b54f828c0d4ba315fc2
Florian Kattner <florian.kattner@gmail.com>
2022-09-15 19:39:15 +0200

    Add dummy to dos

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import "./style.scss";
+
+function Task(name, description, dueDate, priority) {
+    this.name = name
+    this.description = description
+    this.dueDate = dueDate
+    this.priority = priority
+  }
+  
+  Task.prototype.markDone = function() {
+    console.log(this.name)
+  }
+ 
+  let taskList = [];
+
+  function addToTaskList(name, description, dueDate, priority){
+    taskList.push(new Task(name, description, dueDate, priority));
+    console.log(taskList);
+  }
+
+  addToTaskList("Test", "1234", 12-12-12, "High");
